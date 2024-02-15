9c4640ed6ead7349b1f02e8a1caeabab9e88f61f
Loveapp-4ever <160129114+Loveapp-4ever@users.noreply.github.com>
2024-02-15 17:43:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,3 @@ import streamlit
 
 streamlit.tilte('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard Bolied Free-Range Egg')               
-
